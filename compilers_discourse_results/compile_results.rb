23ad0225cc4976cf967f835ae2bc91391907691a
Guo Xiang Tan <tgx_world@hotmail.com>
2015-01-16 11:17:41 +0800

    Update Discourse results to include all percentiles.

--- compilers_discourse_results/compile_results.rb
+++ compilers_discourse_results/compile_results.rb
@@ -2,24 +2,29 @@ require 'csv'
 
 CSV.open('compilers_results.csv', 'w', write_headers: true) do |csv|
   headers = [" "]
-  datas = []
+  datas = {}
 
   Dir["*.txt"].sort.each_with_index do |file, index|
     file.match(/(.+).txt\Z/)
     headers << $1
     results = eval(File.open(file, 'r').read.gsub("NaN", "\"\""))
 
-    %w{categories categories_admin home home_admin topic topic_admin}.each_with_index do |benchmark_type, line_number|
-      datas[line_number] ||= []
-      datas[line_number] << benchmark_type if index == 0
-      data = results[benchmark_type][50]
-      datas[line_number] << data
+    [50, 75, 90, 99].each do |percentile|
+      %w{categories categories_admin home home_admin topic topic_admin}.each_with_index do |benchmark_type, line_number|
+        datas[percentile] ||= []
+        datas[percentile][line_number] ||= []
+        datas[percentile][line_number] << "#{benchmark_type} - #{percentile} percentile" if index == 0
+        data = results[benchmark_type][percentile]
+        datas[percentile][line_number] << data
+      end
     end
   end
 
   csv << headers
 
-  datas.each do |data|
-    csv << data
+  datas.each do |_, value|
+    value.each do |data|
+      csv << data
+    end
   end
 end
